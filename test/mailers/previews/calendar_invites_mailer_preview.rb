796b43237cd6d11b4f7bddf63338805f7dd13b38
Tyler Damme <tyler.damme@gmail.com>
2018-03-22 19:45:41 -0500

    Remove test folder from project
    In preparation for RSpec we removed this unused code

--- test/mailers/previews/calendar_invites_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/calendar_invites_mailer
-class CalendarInvitesMailerPreview < ActionMailer::Preview
-
-end
--- test/models/daily_progress_log_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DailyProgressLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
